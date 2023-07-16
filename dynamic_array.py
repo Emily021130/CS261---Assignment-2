f72b6a95b3e5a6f0219ee4ccdb0a814d59e76f03
Shuyao zeng <zengs@oregonstate.edu>
2023-07-16 15:43:33 -0700

    start trying...

--- dynamic_array.py
+++ dynamic_array.py
@@ -137,7 +137,7 @@ class DynamicArray:
         """
         if new_capacity <= 0 or new_capacity < self._size:
             return
-        new_arr = [None] * new_capacity
+        new_arr = StaticArray(new_capacity)
         for index in range(self._size):
             new_arr[index] = self._data[index]
         self._capacity = new_capacity
