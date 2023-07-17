f58dd4ba74b6cd8844bc2ea8969eb4b5d85e48a4
Shuyao zeng <zengs@oregonstate.edu>
2023-07-16 18:27:37 -0700

    start trying...

--- dynamic_array.py
+++ dynamic_array.py
@@ -169,7 +169,16 @@ class DynamicArray:
         """
         TODO: Write this implementation
         """
-        pass
+        if index < 0 or index > self._size - 1:
+            raise DynamicArrayException
+        if self._capacity > (self._size * 4):
+            self._capacity = self._size * 2
+        if self._capacity < 10:
+            self._capacity = 10
+        self._data[index] = None
+        for i in range(index, self._size - 1):
+            self._data[i] = self._data[i + 1]
+        self._size -= 1
 
     def slice(self, start_index: int, size: int) -> "DynamicArray":
         """
